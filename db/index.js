1549563d2913a92916dd4415181d01148595d1d1
Johnny Hill <jvhill728@gmail.com>
2022-03-30 21:01:02 -0500

    creating updateUser function

--- db/index.js
+++ db/index.js
@@ -34,6 +34,37 @@ async function getAllUsers() {
     return rows;
 }
 
+async function updateUser(id, fields = {}) {
+  //build the set string
+
+  const setString = Object.keys(fields).map(
+    (key, index) => `"${ key }"=$${ index + 1}`
+  ).join(', ');
+
+
+  //return early if this is called without fields
+  if (setString.length === 0) {
+    return;
+  }
+
+  try {
+    const result = await client.query(`
+      UPDATE users
+      SET ${ setString }
+      WHERE id=${ id }
+      RETURNING *
+      `, Object.values(fields));
+
+
+    return result;  
+  } catch (error) {
+    throw error;
+  }
+
+
+
+}
+
 module.exports = {
   client,
   createUser,
