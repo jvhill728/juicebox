91a94de7be42678501ffb406ac5aa59a10175f40
Johnny Hill <jvhill728@gmail.com>
2022-04-09 10:23:37 -0500

    starting api

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ const PORT = 3000;
 const express = require('express');
 const server = express();
 
+server.use((req, res, next) => {
+  console.log("<____Body Logger START____>");
+  console.log(req.body);
+  console.log("<____Body Logger END____>");
+
+  next();
+});
+
 
 server.listen(PORT, () => {
   console.log('The server is up on port', PORT)
