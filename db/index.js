18dc2b206d720a3437a518f7baf2313813feb458
Johnny Hill <jvhill728@gmail.com>
2022-04-10 17:37:36 -0500

    heroku changes

--- db/index.js
+++ db/index.js
@@ -2,7 +2,7 @@ const { Client } = require('pg'); //imports pg module
 const { rows } = require('pg/lib/defaults');
 
 //supply the db name and location of the database
-const client = new Client('postgres://localhost:5432/juicebox-dev');
+const client = new Client(process.env.DATABASE_URL || 'postgres://localhost:5432/juicebox-dev');
 
 
 async function createUser({ 
